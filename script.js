3d7d5734ec58c44cb1cbc29d92c08ea7c4784eba
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-03-04 19:04:39 -0500

    Variables
    Anatomy of a variable and differences between 'let', 'var' and 'const'

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+let box = "Woody";
+console.log(box);
+
+const pi = 3.14;
+console.log(pi);
+
+var number = 10;
+var number = 12;
+console.log(number);
+
+let letter = "A";
+letter = "B";
+console.log(letter);
+
+// 'let' and 'const' have block scope, while 'var' has function scope
+// 'var' can be re-declared and reassigned
+// 'const' cannot be re-declared nor reassigned
