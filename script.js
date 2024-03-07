0fb2d4f5962a645bf8cb92ca501da25922ee1e3e
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-03-07 14:42:39 -0500

    String creation

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-let fruits = {
-  orange: "🍊",
-};
+// Strings
+let firstOption = 'Single quotes';
+let secondOption = "Double quotation marks";
+let thirdOption = `Grave accent`;
 
-let vegetables = fruits;
-vegetables.orange = "🥦";
-console.log(fruits);
+// Concatenation (+)
+let address = "Cra. 123 #45-12";
+let city = "Colombia";
+let fullAddress = "My address is " + address + city;
+fullAddress;
+let addressWithoutSpace = "My address is " + address + " " + city;
+addressWithoutSpace;
 
-let clothes = { coat: "🧥" }; // unused
-clothes = { jeans: "👖" }; // new memory reference
-clothes;
+// Concatenation (Template literals)
+let firstName = "Johan Rodriguez";
+let country = "CO";
+let profile = `My profile says "${firstName} - ${country}"`;
+profile;
 
-// The garbage collector collects and deletes memory references that we cannot access
+// Concatenation (join())
+let idea1 = "I love";
+let idea2 = "Korean";
+let idea3 = "people";
+let thoughts = [idea1, idea2, idea3];
+console.log(thoughts.join("🥢"));
+
+// Concatenation (concat())
+let hobby1 = "watching movies or series";
+let hobby2 = "listening to music";
+let hobby3 = "playing video games";
+let hobbies = "My hobbies are: ".concat(hobby1, ", ", hobby2, ", and ", hobby3, ".");
+hobbies;
