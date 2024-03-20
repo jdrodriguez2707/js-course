4278dcdec308c85ac66eb677ac2475de34acda1f
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-03-20 14:50:23 -0500

    Anatomy of a function

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
-// Social media profile
+function calculateDiscountedPrice(price, discountPercentage) {
+  let discount = (price * discountPercentage) / 100;
+  let priceWithDiscount = price - discount;
 
-// 1. User information
-let userName = "rafitaDEV";
-let fullName = "Johan Rodriguez";
-let age = 19;
-let isStudent = true;
+  return priceWithDiscount;
+}
 
-// 2. Address
-const address = {
-  street: "26",
-  city: "Barranquilla",
-  department: "Atlántico",
-  zipCode: "080005"
-};
+let originalPrice = 100;
+let discountPercentage = 20;
+let finalPrice = calculateDiscountedPrice(originalPrice, discountPercentage);
 
-// 3. Hobbies
-const hobbies = ["watching movies and series", "playing video games", "listening to music"];
-
-// 4. Generating personalized bio
-let personalizedBio = `Hi there!👋🏻 My name's ${fullName}, but you can also call me ${userName} 😉.
-I'm ${age} years old and it's ${isStudent} that I'm a student 🤓.
-I live in ${address.city}, ${address.department} ☀️. 
-I love ${hobbies[0]}, ${hobbies[1]} and ${hobbies[2]} 🎥🎮🎧. 
-Follow me if you have the same hobbies. I hope to meet you someday.
-Have a nice day 😎.`;
-
-// Print user profile 
-console.log(personalizedBio);
+console.log(`Original price: $${originalPrice}
+Discount: ${discountPercentage}%
+Price with discount: $${finalPrice}`);
