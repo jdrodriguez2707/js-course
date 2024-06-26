4da730bac54d43be17bc7d91682bbb207507fc70
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-06-26 14:22:12 -0500

    Project: Guess the secret word

--- script.js
+++ script.js
@@ -1,17 +1,74 @@
-alert("Enter a range number to guess");
-const min = parseInt(prompt("Enter the minimum value:"));
-const max = parseInt(prompt("Enter the maximum value:"));
-let userNumber;
-const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-console.log(randomNumber);
+// Game: Guess the secret word
 
-do {
-  userNumber = parseInt(
-    prompt(`Try to guess now. Enter a number between ${min} and ${max}:`)
-  );
+alert(`Welcome to the game: Guess the secret word! 🧐\n
+The challenge is to guess a secret word.
+You only have three attempts.`);
+
+const words = [
+  { platzi: "A platform to learn programming." },
+  { javascript: "A programming language." },
+  { programming: "The act of writing code." },
+  { developer: "A person who writes code." },
+  { frontend: "The part of the website that users interact with." },
+  { backend: "The part of the website that users don't see." },
+  { fullstack: "A developer who works on both frontend and backend." },
+  { code: "Instructions that a computer can understand." },
+  { algorithm: "A set of instructions to solve a problem." },
+  { database: "A place to store data." },
+];
+const secretWordObject = getRandomWord(words);
+const TOTAL_ATTEMPTS = 3;
+
+startGame();
+
+function getRandomWord(wordList) {
+  const randomIndex = Math.floor(Math.random() * wordList.length);
+  return wordList[randomIndex];
+}
+
+function startGame() {
+  let userSupposition;
+  const secretWord = Object.keys(secretWordObject)[0];
+  const hint = Object.values(secretWordObject)[0];
+
+  for (let attempts = 1; attempts <= TOTAL_ATTEMPTS; attempts++) {
+    userSupposition = prompt(
+      `Let's get started! 🙂\nTry to guess the secret word 🔐\nHint: ${hint}`
+    );
+
+    if (!userSupposition) {
+      alert("Please enter a guess.");
+      attempts--;
+      continue;
+    }
+
+    if (userSupposition.toLowerCase() === secretWord) {
+      alert(`You're right! That's the word: '${secretWord}'`);
+      break;
+    } else {
+      alert(
+        `You're wrong. That's not the word.\n${
+          attempts < TOTAL_ATTEMPTS
+            ? `You have ${TOTAL_ATTEMPTS - attempts} attempts left.`
+            : `You have no more attempts left.`
+        }`
+      );
+    }
+  }
+
   alert(
-    userNumber === randomNumber
-      ? "Congratulations! You guessed the number"
-      : `Fail :( Try again`
+    userSupposition.toLowerCase() === secretWord
+      ? "Congratulations! You did it 🎉"
+      : `You lost ☹️. The word was '${secretWord}'. Good luck next time`
   );
-} while (userNumber !== randomNumber);
+
+  restartGame();
+}
+
+function restartGame() {
+  if (confirm("Do you want to play again?")) {
+    location.reload();
+  } else {
+    alert("Thanks for playing! 👋");
+  }
+}
