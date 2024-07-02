b80752c254bf7a2811b867463e4405dd180e16d8
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-07-02 18:39:10 -0500

    Project: Transaction analysis

--- script.js
+++ script.js
@@ -1,25 +1,74 @@
-// reduce method to know the number of times a word appears in a string
-
-const fruits = [
-  "apple",
-  "banana",
-  "pineapple",
-  "apple",
-  "apple",
-  "banana",
-  "pineapple",
-  "pineapple",
-  "strawberry",
+const transactions = [
+  { id: 1, description: "Salary", amount: 3000 },
+  { id: 2, description: "Clothes purchase", amount: -150 },
+  { id: 3, description: "Car sale", amount: 5000 },
+  { id: 4, description: "Food purchase", amount: -200 },
+  { id: 5, description: "Services pay", amount: -100 },
+  { id: 6, description: "Extra income", amount: 400 },
+  { id: 7, description: "Furniture purchase", amount: -800 },
+  { id: 8, description: "Bicycle sale", amount: 150 },
+  { id: 9, description: "Taxes devolution", amount: 700 },
+  { id: 10, description: "Purchase of books", amount: -50 },
 ];
 
-const fruitFrequency = fruits.reduce((accumulator, currentValue) => {
-  if (accumulator[currentValue]) {
-    accumulator[currentValue]++;
-  } else {
-    accumulator[currentValue] = 1;
-  }
+const calculateTotalBalance = (transactions) =>
+  transactions.reduce(
+    (accumulator, currentTransaction) =>
+      accumulator + currentTransaction.amount,
+    0
+  );
 
-  return accumulator;
-}, {});
+function findLargestTransaction(transactions) {
+  const largestTransaction = transactions.reduce(
+    (largest, currentTransaction) => {
+      if (Math.abs(currentTransaction.amount) > Math.abs(largest.amount)) {
+        return currentTransaction;
+      }
+      return largest;
+    },
+    transactions[0]
+  );
 
-console.log(fruitFrequency);
+  return largestTransaction;
+}
+
+const showPurchaseTransactions = (transactions) =>
+  transactions.filter((transaction) => transaction.amount < 0);
+
+const findTransactionByDescription = (transactions, targetDescription) =>
+  transactions.find(
+    (transaction) =>
+      targetDescription.toLowerCase() === transaction.description.toLowerCase()
+  );
+
+const findTransactionIndexByAmount = (transactions, targetAmount) =>
+  transactions.findIndex((transaction) => targetAmount === transaction.amount);
+
+function updateTransactionDescriptions(transactions) {
+  transactions.forEach((transaction) => {
+    transaction.description =
+      transaction.amount < 0
+        ? `Spent: ${transaction.description}`
+        : `Income: ${transaction.description}`;
+  });
+
+  return transactions;
+}
+
+console.log(`Total balance: ${calculateTotalBalance(transactions)}`);
+console.log("The largest transaction:", findLargestTransaction(transactions));
+console.log("Purchase transactions:", showPurchaseTransactions(transactions));
+console.log(
+  "Transaction found by description:",
+  findTransactionByDescription(transactions, "furniture purchase")
+);
+console.log(
+  `Transaction index found by amount: ${findTransactionIndexByAmount(
+    transactions,
+    -150
+  )}`
+);
+console.log(
+  "Updated transaction descriptions:",
+  updateTransactionDescriptions(transactions)
+);
