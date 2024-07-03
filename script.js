14ec42ccb35306f207c8ef84c7322cbb0d8349a1
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-07-03 10:52:40 -0500

    Spread operator: Use cases

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
-// Methods that DO NOT modify the original array (Immutability).
-
-const morseCode1 = ["....", "---"]; // H O
-const morseCode2 = [".-..", ".-"]; // L A
-
-// concat() way 1
-const morseCodeMessage = morseCode1.concat(morseCode2);
-console.log(morseCode1);
-console.log(morseCode2);
-console.log(morseCodeMessage);
-
-// concat() way 2
-const morseCodeMessage2 = [].concat(morseCode1, morseCode2);
-console.log(morseCode1);
-console.log(morseCode2);
-console.log(morseCodeMessage);
-
-// Spread operator
-function combineMorseMessages(morseCode1, morseCode2) {
-  console.log([...morseCode1, ...morseCode2]);
+function addNumbers(...numbers) {
+  return numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber, 0)
 }
 
-combineMorseMessages(morseCode1, morseCode2);
-
-const numbers = [1, 2, 3];
-const string = "string";
-
-combineMorseMessages(numbers, string);
-
-// join()
-const morseCodeMessageString = morseCodeMessage.join("");
-console.log(morseCodeMessageString);
+console.log(addNumbers(22, 22))
