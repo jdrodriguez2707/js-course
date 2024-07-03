b5c82ca7479c51e58501bc3dec9d5b9dfcac1302
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-07-03 11:23:19 -0500

    Bidimensional arrays

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-function addNumbers(...numbers) {
-  return numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber, 0)
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+// let value = matrix[0][1]
+// console.log(value)
+
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    console.log(matrix[i][j])
+  }
 }
 
-console.log(addNumbers(22, 22))
+function findElement(matrix, element) {
+  for (let i = 0; i < matrix.length; i++){
+    for (let j = 0; j < matrix[i].length; j++){
+      if (matrix[i][j] === element) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+console.log(findElement(matrix, 5))
+
+// shallow copy
+let newMatrix = [...matrix];
+console.log(newMatrix);
+
+newMatrix[0][0] = 10;
+console.log(matrix);
+console.log(newMatrix); 
+
+
+// deep copy
+function duplicateMatrix(matrix) {
+  let newMatrix = []
+  for (let i = 0; i < matrix.length; i++) {
+    newMatrix[i] = [...matrix[i]]
+  }
+  return newMatrix
+}
+const matrixWithDeepCopy = duplicateMatrix(matrix)
+console.log(matrixWithDeepCopy);
+
+matrix[0][2] = -1;
+console.log(matrix);
+console.log(matrixWithDeepCopy); 
